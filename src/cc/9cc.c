4ea7e818e2ed031573767297c50c10ee16bdc408
na2 <51122019+na2shell@users.noreply.github.com>
2021-10-17 15:54:19 +0900

    step4: エラ-メッセージを作成する関数を追加

--- src/cc/9cc.c
+++ src/cc/9cc.c
@@ -14,7 +14,8 @@ typedef enum
 
 typedef struct Token Token;
 
-struct Token {
+struct Token
+{
     TokenKind kind;
     Token *next;
     int val;
@@ -22,80 +23,104 @@ struct Token {
 };
 
 Token *token;
+char *user_input;
 
-void error(char *fmt, ...){
+void error_at(char *loc, char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
-    vfprintf(stderr,fmt,ap);
-    fprintf(stderr,"\n");
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
     exit(1);
 };
 
-bool consume(char op){
+bool consume(char op)
+{
     if (token->kind != TK_RESERVED || token->str[0] != op)
-      return false;
+        return false;
     token = token->next;
     return true;
 };
 
-void expect(char op){
+void expect(char op)
+{
     if (token->kind != TK_RESERVED || token->str[0] != op)
-      error("'%c'ではありません",op);
-      token = token->next;
+        error_at(token->str, "'%c'ではありません", op);
+    token = token->next;
 };
 
-int expect_number(){
-    if(token->kind != TK_NUM)
-      error("数ではありません");
-    
+int expect_number()
+{
+    if (token->kind != TK_NUM)
+        error_at(token->str, "数ではありません");
+
     int val = token->val;
     token = token->next;
     return val;
 };
 
-bool at_eof(){
+bool at_eof()
+{
     return token->kind == TK_EOF;
 };
 
-Token *new_token(TokenKind kind, Token *cur, char *str){
-    Token *tok = calloc(1,sizeof(Token));
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+    Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
     cur->next = tok;
     return tok;
 };
 
-Token *tokenize(char *p){
+Token *tokenize()
+{
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
 
-    while (*p){
-        if (isspace(*p)){
+    while (*p)
+    {
+        if (isspace(*p))
+        {
             p++;
             continue;
         }
 
-        if (*p == '+' || *p == '-'){
-            cur = new_token(TK_RESERVED,cur,p++);
+        if (*p == '+' || *p == '-')
+        {
+            cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
 
-        if(isdigit(*p)){
-            cur = new_token(TK_NUM,cur,p);
-            cur->val = strtol(p,&p,10);
+        if (isdigit(*p))
+        {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
             continue;
         }
 
-        error("トークナイズ不可");
+        error_at(p,"トークナイズ不可");
     }
 
-    new_token(TK_EOF,cur,p);
+    new_token(TK_EOF, cur, p);
     return head.next;
 }
-
-
-
 
 int main(int argc, char **argv)
 {
@@ -105,19 +130,19 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    token = tokenize(argv[1]);
-
+    user_input = argv[1];
+    token = tokenize();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-
     printf("  mov rax, %d\n", expect_number());
 
     while (!at_eof())
     {
-        if (consume('+')){
+        if (consume('+'))
+        {
             printf("  add rax, %d\n", expect_number());
             continue;
         }
